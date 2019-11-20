e094ac51e3031e1b84f9c89a51457c329b440d63
Wes-White <wesleyrwhite86@gmail.com>
2019-11-19 17:56:16 -1000

    Added Redux store, moved state to new reducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from "react";
-import { createStore } from "redux";
-
 import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
+import { intialState } from "./reducers/reducer";
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: "2019 Ford Mustang",
-      image:
-        "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: "V-6 engine", price: 1500 },
-      { id: 2, name: "Racing detail package", price: 1500 },
-      { id: 3, name: "Premium sound system", price: 500 },
-      { id: 4, name: "Rear spoiler", price: 250 }
-    ]
-  };
-
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
@@ -35,12 +17,17 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={intialState.car} />
+        <AddedFeatures car={intialState.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures
+          additionalFeatures={intialState.additionalFeatures}
+        />
+        <Total
+          car={intialState.car}
+          additionalPrice={intialState.additionalPrice}
+        />
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { reducer } from "./reducers/reducer";
 
 import "bulma/css/bulma.css";
 import "./styles.scss";
 
+const appStore = createStore(reducer);
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Provider store={appStore}>
-    <App />, rootElement
-  </Provider>
+    <App />
+  </Provider>,
+  rootElement
 );
